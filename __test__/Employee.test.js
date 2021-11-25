060330b4c6e7176da629c124e5b1fc4781669089
Adam Field <adamjfield@gmail.com>
2021-11-25 16:56:48 -0600

    finish employee and employee tests

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -9,7 +9,25 @@ test("creates an employee object", () => {
 });
 
 test("gets name from getName", () => {
-  const employee = new Employee("Adam", "adamjfield@gmail.com");
+  const employee = new Employee("Adam", 10, "adamjfield@gmail.com");
 
   expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test("gets ID from getId", () => {
+  const employee = new Employee("Adam", 10, "adamjfield@gmail.com");
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test("gets email from getEmail", () => {
+  const employee = new Employee("Adam", 10, "adamjfield@gmail.com");
+
+  expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test("gets role from getRole", () => {
+  const employee = new Employee("Adam", 10, "adamjfield@gmail.com");
+
+  expect(employee.getRole()).toEqual("Employee");
 });
