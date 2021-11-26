4d692398685cd6a8d74f376f0cd14740518670ab
Adam Field <adamjfield@gmail.com>
2021-11-25 18:14:00 -0600

    add tests for engineer

---
+++ __test__/Engineer.test.js
@@ -0,0 +1,34 @@
+const Engineer = require("../lib/Engineer");
+
+test("creates an engineer object", () => {
+  const engineer = new Engineer(
+    "Adam",
+    10,
+    "adamjfield@gmail.com",
+    "johndoe99"
+  );
+
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+test("gets github username from getGithub", () => {
+  const engineer = new Engineer(
+    "Adam",
+    10,
+    "adamjfield@gmail.com",
+    "johndoe99"
+  );
+
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+test("gets role from getRole", () => {
+  const engineer = new Engineer(
+    "Adam",
+    10,
+    "adamjfield@gmail.com",
+    "johndoe99"
+  );
+
+  expect(engineer.getRole()).toEqual("Engineer");
+});
